b6447a4c5747b85eab9972e191982d4619bb6004
matheusafd <matheus.daf@outlook.com>
2022-08-20 22:36:25 -0300

    fix aos

--- src/util/aos-scroll.js
+++ src/util/aos-scroll.js
@@ -1,6 +1,6 @@
-import AOS from "aos";
-import "aos/dist/aos.css";
+// import AOS from "aos";
+// import "aos/dist/aos.css";
 
-AOS.init({
-  disable: "mobile",
-});
+// AOS.init({
+//   disable: "mobile",
+// });
